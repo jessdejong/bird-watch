c4d12c20dc6dda34cd27bec3d6cdb1b06d732391
Sinister-Paradox <alexdejong737@gmail.com>
2018-02-23 22:27:20 -0600

    Separated Functions

--- twitter_wrapper.py
+++ twitter_wrapper.py
@@ -1,24 +1,19 @@
 from twitter import *
-import json, string
+import json
 
-# twitter keys
 ACCESS_TOKEN = "967202509990322176-WoUrv9EqLk6cdkFjkXfuZhvufdA6ysb"
 ACCESS_SECRET = "65xf901IAzmi8xjOUEf2Vr8oinPBp51J25Bt4PbtFAfnE"
 CONSUMER_KEY = "CS0DjdYG1kOP5XmSP9cVudJdX"
 CONSUMER_SECRET = "DoVIxtMN4wETttNCfjiAnRyRMK9EL1CrLLtUXi5SroqjTGUAnx"
-printable = set(string.printable)
 
-# get all of user tweets
 def getUserTweets(username):
     t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
     data = json.loads(json.dumps(t.statuses.user_timeline(screen_name=username, count=200)))
     tweets = [];
     for i in range(len(data)):
         tweets.append(data[i]["text"].encode('ascii',errors='ignore').decode())
-        links.append("https://www.twitter.com/"+username+"/status/"+str(data[i]["id"]))
     return tweets, links
     
-# get list of followers
 def getUserFollowers(username):
     t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
     data = json.loads(json.dumps(t.friends.list(screen_name=username, count=200)))
@@ -26,3 +21,14 @@ def getUserFollowers(username):
     for i in range(len(data["users"])):
         friends.append(data["users"][i]["screen_name"])
     return friends
+
+def getUserLinks(username):
+    t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
+    data = json.loads(json.dumps(t.statuses.user_timeline(screen_name=username, count=200)))
+    links = [];
+    for i in range(len(data)):
+        links.append("https://www.twitter.com/"+username+"/status/"+str(data[i]["id"]))
+    return tweets, links
+
+#print(getUserFollowers("SyedIsSoHot"))
+print(getUserTweets("SyedIsSoHot"))
