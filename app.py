2e2ce50ced1186b09c1215323dc246bd0182ae21
Jess DeJong <jessdejong843@gmail.com>
2018-02-23 20:34:09 -0600

    new file:   __pycache__/app.cpython-36.pyc 	modified:   app.py

--- app.py
+++ app.py
@@ -2,5 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World'
+def index():
+    return 'Index Page'
+
+@app.route('user/<username>')
+def get_user(username):
+    # get the users tweets
+
+
+# STUFF WE NEED TO DO:
+#potential tweets that are negative
+#potential users that are negative
